b1bb9cb4e4411b1397b2c77c650577cdcbd8e955
Anders Sjöberg <anders.sjoberg@calabrio.com>
2019-09-03 14:28:31 +0200

    Start

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,30 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Nav from "../components/Nav.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+    display: grid;
+    grid-template-columns: [left] 20% [main] auto [right] 10%;
+  }
+
+  .left {
+    grid-area: "left";
+  }
 </style>
 
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot name="left" class="left" />
+  <slot name="main" class="main" />
+  <slot name="right" class="right" />
 </main>
--- src/routes/button_doc.svelte
+++ src/routes/button_doc.svelte
@@ -10,29 +10,30 @@
   }
 </style>
 
-<h1>Button</h1>
-<section>
-  <div>
-    <Button onClick={() => (size = 'large')}>Large</Button>
-    <Button onClick={() => (size = 'default')}>Default</Button>
-    <Button onClick={() => (size = 'small')}>Small</Button>
-    <br />
-    <br />
-    <Button type="primary" {size}>Primary</Button>
-    <Button {size}>Normal</Button>
-    <Button type="dashed" {size}>Dashed</Button>
-    <Button type="danger" {size}>Danger</Button>
-    <Button type="link" {size}>Link</Button>
-    <br />
-    <Button type="primary" icon="download" {size} />
-    <Button type="primary" shape="circle" icon="download" {size} />
-    <Button type="primary" shape="round" icon="download" {size} />
-    <Button type="primary" shape="round" icon="download" {size}>
-      Download
-    </Button>
-    <Button type="primary" icon="download" {size}>Download</Button>
-    <br />
-    <!-- <Button.Group {size}>
+<div slot="main">
+  <h1>Button</h1>
+  <section>
+    <div>
+      <Button onClick={() => (size = 'large')}>Large</Button>
+      <Button onClick={() => (size = 'default')}>Default</Button>
+      <Button onClick={() => (size = 'small')}>Small</Button>
+      <br />
+      <br />
+      <Button type="primary" {size}>Primary</Button>
+      <Button {size}>Normal</Button>
+      <Button type="dashed" {size}>Dashed</Button>
+      <Button type="danger" {size}>Danger</Button>
+      <Button type="link" {size}>Link</Button>
+      <br />
+      <Button type="primary" icon="download" {size} />
+      <Button type="primary" shape="circle" icon="download" {size} />
+      <Button type="primary" shape="round" icon="download" {size} />
+      <Button type="primary" shape="round" icon="download" {size}>
+        Download
+      </Button>
+      <Button type="primary" icon="download" {size}>Download</Button>
+      <br />
+      <!-- <Button.Group {size}>
       <Button type="primary">
         <Icon type="left" />
         Backward
@@ -42,5 +43,6 @@
         <Icon type="right" />
       </Button>
     </Button.Group> -->
-  </div>
-</section>
+    </div>
+  </section>
+</div>
