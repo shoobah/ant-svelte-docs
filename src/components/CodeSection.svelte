23b87616b14f43272a3c0d9bb47b23170a2ab649
shoobah <shoobah@gmail.com>
2019-09-08 02:24:40 +0200

    Code sections

---
+++ src/components/CodeSection.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { slide } from "svelte/transition";
+  import { Icon } from "ant-svelte";
+
+  export let code = "";
+
+  let display = false;
+</script>
+
+<style>
+  .code-section {
+    border-top: solid 1px #ccc;
+    border-bottom: solid 1px #ccc;
+    margin: 1em 0;
+  }
+</style>
+
+<div style="cursor:pointer" on:click={() => (display = !display)}>
+  Show code
+  <Icon flip={display} type="up" theme="outline" size="1em" />
+</div>
+{#if display}
+  <div class="code-section" transition:slide>{code}</div>
+{/if}
--- src/routes/button_doc.svelte
+++ src/routes/button_doc.svelte
@@ -1,8 +1,13 @@
 <script>
   import { Button, Icon } from "ant-svelte";
   import Main from "../components/Main.svelte";
+  import CodeSection from "../components/CodeSection.svelte";
 
   let size = "default";
+
+  const codeSection_0 = `<Button onClick={() => (size = 'large')}>Large</Button>
+      <Button onClick={() => (size = 'default')}>Default</Button>
+      <Button onClick={() => (size = 'small')}>Small</Button>`;
 </script>
 
 <style>
@@ -16,27 +21,40 @@
 </svelte:head>
 
 <h1>Button</h1>
+<h2>Usage</h2>
+<p />
+
 <section>
   <div>
-    <Button onClick={() => (size = 'large')}>Large</Button>
-    <Button onClick={() => (size = 'default')}>Default</Button>
-    <Button onClick={() => (size = 'small')}>Small</Button>
-    <br />
-    <br />
-    <Button type="primary" {size}>Primary</Button>
-    <Button {size}>Normal</Button>
-    <Button type="dashed" {size}>Dashed</Button>
-    <Button type="danger" {size}>Danger</Button>
-    <Button type="link" {size}>Link</Button>
-    <br />
-    <Button type="primary" icon="download" {size} />
-    <Button type="primary" shape="circle" icon="download" {size} />
-    <Button type="primary" shape="round" icon="download" {size} />
-    <Button type="primary" shape="round" icon="download" {size}>
-      Download
-    </Button>
-    <Button type="primary" icon="download" {size}>Download</Button>
-    <br />
+    <div>
+      <Button onClick={() => (size = 'large')}>Large</Button>
+      <Button onClick={() => (size = 'default')}>Default</Button>
+      <Button onClick={() => (size = 'small')}>Small</Button>
+    </div>
+    <CodeSection code={codeSection_0} />
+    <div>
+      <Button type="primary" {size}>Primary</Button>
+      <Button {size}>Normal</Button>
+      <Button type="dashed" {size}>Dashed</Button>
+      <Button type="danger" {size}>Danger</Button>
+      <Button type="link" {size}>Link</Button>
+    </div>
+    <div>
+      <Button type="primary" icon="download" {size} />
+      <Button type="primary" shape="circle" icon="download" {size} />
+      <Button type="primary" shape="round" icon="download" {size} />
+      <Button type="primary" shape="round" icon="download" {size}>
+        Download
+      </Button>
+      <Button type="primary" icon="download" {size}>Download</Button>
+    </div>
+    <div>
+      <Button type="primary" loading {size} />
+      <Button type="primary" shape="circle" loading {size} />
+      <Button type="primary" shape="round" loading {size} />
+      <Button type="primary" shape="round" loading {size}>Download</Button>
+      <Button type="primary" loading {size}>Download</Button>
+    </div>
     <!-- <Button.Group {size}>
       <Button type="primary">
         <Icon type="left" />
